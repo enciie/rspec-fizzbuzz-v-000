22271b18890dd736da5b2850e0b5d42a65f92d8b
Nancy Jo <enciie@gmail.com>
2018-06-01 01:27:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
-  end
+ elsif int % 5 == 0
+  return "Buzz"
+end
   # elsif int divisible by 5 equals Buzz
   #
   # elsif int divisible by 15 equals FizzBuzz
   #
   # else
   # returns nil
-end
